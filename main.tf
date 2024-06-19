862a156af75fbcf9f430c3a18ff71a093b976227
officeguy <marty.lindsay@gmail.com>
2024-06-19 17:15:50 +1200

    7

--- main.tf
+++ main.tf
@@ -53,29 +53,23 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+ target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = [
-        {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      ]
     }
-  }
+  ]
 
 
-  listeners = [
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-    ]
+  ]
   
 
   tags = {
